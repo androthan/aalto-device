d3719041578419cf74a05000ee8ab4f90382bfa5
Jonathan Grundmann <jonathan0android@gmail.com>
2015-03-12 23:13:50 +0100

    POWER: new power manager

--- power/Android.mk
+++ power/Android.mk
@@ -19,13 +19,9 @@ LOCAL_PATH := $(call my-dir)
 # hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := power-aalto.c
-
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-
 LOCAL_SHARED_LIBRARIES := liblog libcutils
-
+LOCAL_SRC_FILES := power_latona.c
 LOCAL_MODULE := power.aalto
 LOCAL_MODULE_TAGS := optional
-
 include $(BUILD_SHARED_LIBRARY)
--- power/power-aalto.c
+++ power/power-aalto.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (C) 2012 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,27 +14,29 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/* Adapted from:
+ * https://github.com/dhiru1602/android_device_samsung_galaxysl/blob/cm-11.0/power/power_latona.c
+ * Thanks to:
+ * dhiru1602
+ * sconosciuto
+ * The CyanogenMod Project
+ */
 #include <errno.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#define LOG_TAG "YP-GS1 PowerHAL"
+#define LOG_TAG "aalto PowerHAL"
 #include <utils/Log.h>
 
 #include <hardware/hardware.h>
 #include <hardware/power.h>
 
 #define CPUFREQ_CPU0 "/sys/devices/system/cpu/cpu0/cpufreq/"
-#define CPUFREQ_ONDEMAND "/sys/devices/system/cpu/cpufreq/ondemandboost/"
-#define BOOSTPULSE_ONDEMAND (CPUFREQ_ONDEMAND "boostpulse")
-
-#define MAX_BUF_SZ  10
-
-/* initialize to something safe */
-static char screen_off_max_freq[MAX_BUF_SZ] = "600000";
-static char scaling_max_freq[MAX_BUF_SZ] = "1100000";
+#define CPUFREQ_ONDEMAND "/sys/devices/system/cpu/cpufreq/ondemand/"
+#define CPUFREQ_INTERACTIVE "/sys/devices/system/cpu/cpufreq/interactive/"
 
 struct aalto_power_module {
     struct power_module base;
@@ -41,6 +44,36 @@ struct aalto_power_module {
     int boostpulse_fd;
     int boostpulse_warned;
 };
+
+static char governor[20];
+
+static int sysfs_read(char *path, char *s, int num_bytes)
+{
+    char buf[80];
+    int count;
+    int ret = 0;
+    int fd = open(path, O_RDONLY);
+
+    if (fd < 0) {
+        strerror_r(errno, buf, sizeof(buf));
+        ALOGE("Error opening %s: %s\n", path, buf);
+
+        return -1;
+    }
+
+    if ((count = read(fd, s, num_bytes - 1)) < 0) {
+        strerror_r(errno, buf, sizeof(buf));
+        ALOGE("Error writing to %s: %s\n", path, buf);
+
+        ret = -1;
+    } else {
+        s[count] = '\0';
+    }
+
+    close(fd);
+
+    return ret;
+}
 
 static void sysfs_write(char *path, char *s)
 {
@@ -63,27 +96,44 @@ static void sysfs_write(char *path, char *s)
     close(fd);
 }
 
-
-int sysfs_read(const char *path, char *buf, size_t size)
-{
-    int fd, len;
-
-    fd = open(path, O_RDONLY);
-    if (fd < 0)
+static int get_scaling_governor() {
+    if (sysfs_read(CPUFREQ_CPU0 "scaling_governor", governor,
+                sizeof(governor)) == -1) {
+        // Can't obtain the scaling governor. Return.
         return -1;
+    } else {
+        // Strip newline at the end.
+        int len = strlen(governor);
 
-    do {
-        len = read(fd, buf, size);
-    } while (len < 0 && errno == EINTR);
+        len--;
 
-    close(fd);
+        while (len >= 0 && (governor[len] == '\n' || governor[len] == '\r'))
+            governor[len--] = '\0';
+    }
 
-    return len;
+    return 0;
 }
 
-static void aalto_power_init(struct power_module *module)
+static void aalto_power_set_interactive(struct power_module *module, int on)
 {
-    sysfs_write(CPUFREQ_ONDEMAND "boostfreq", "800000");
+    get_scaling_governor();
+    if (strncmp(governor, "interactive", 11) == 0)
+        sysfs_write(CPUFREQ_INTERACTIVE "timer_rate", on ? "30000" : "150000");
+    if (strncmp(governor, "ondemand", 8) == 0)
+        sysfs_write(CPUFREQ_ONDEMAND "sampling_rate", on ? "60000" : "150000");
+}
+
+static void configure_governor()
+{
+    aalto_power_set_interactive(NULL, 1);
+    if (strncmp(governor, "interactive", 11) == 0) {
+        sysfs_write(CPUFREQ_INTERACTIVE "min_sample_time", "90000");
+        sysfs_write(CPUFREQ_INTERACTIVE "above_hispeed_delay", "30000");
+        sysfs_write(CPUFREQ_INTERACTIVE "hispeed_freq", "600000");
+    } else if (strncmp(governor, "ondemand", 8) == 0) {
+        sysfs_write(CPUFREQ_ONDEMAND "io_is_busy", "1");
+        sysfs_write(CPUFREQ_ONDEMAND "boostfreq", "600000");
+    }
 }
 
 static int boostpulse_open(struct aalto_power_module *aalto)
@@ -93,47 +143,28 @@ static int boostpulse_open(struct aalto_power_module *aalto)
     pthread_mutex_lock(&aalto->lock);
 
     if (aalto->boostpulse_fd < 0) {
-        aalto->boostpulse_fd = open(BOOSTPULSE_ONDEMAND, O_WRONLY);
-
-        if (aalto->boostpulse_fd < 0 && !aalto->boostpulse_warned) {
-            strerror_r(errno, buf, sizeof(buf));
-            ALOGE("Error opening boostpulse: %s\n", buf);
+        if (get_scaling_governor() < 0) {
+            ALOGE("Can't read scaling governor.");
             aalto->boostpulse_warned = 1;
-        } else if (aalto->boostpulse_fd > 0)
-            ALOGD("Opened boostpulse interface\n");
+        } else {
+            if (strncmp(governor, "ondemand", 8) == 0)
+                aalto->boostpulse_fd = open(CPUFREQ_ONDEMAND "boostpulse", O_WRONLY);
+            else if (strncmp(governor, "interactive", 11) == 0)
+                aalto->boostpulse_fd = open(CPUFREQ_INTERACTIVE "boostpulse", O_WRONLY);
+
+            if (aalto->boostpulse_fd < 0 && !aalto->boostpulse_warned) {
+                strerror_r(errno, buf, sizeof(buf));
+                ALOGE("Error opening %s boostpulse interface: %s\n", governor, buf);
+                aalto->boostpulse_warned = 1;
+            } else if (aalto->boostpulse_fd > 0) {
+                configure_governor();
+                ALOGD("Opened %s boostpulse interface", governor);
+            }
+        }
     }
 
     pthread_mutex_unlock(&aalto->lock);
     return aalto->boostpulse_fd;
-}
-
-static void aalto_power_set_interactive(struct power_module *module, int on)
-{
-    return;
-
-    int len;
-
-    char buf[MAX_BUF_SZ];
-
-    /*
-     * Lower maximum frequency when screen is off.  
-     */
-    if (!on) {
-        /* read the current scaling max freq and save it before updating */
-        len = sysfs_read(CPUFREQ_CPU0 "scaling_max_freq", buf, sizeof(buf));
-
-        /* make sure it's not the screen off freq, if the "on"
-         * call is skipped (can happen if you press the power
-         * button repeatedly) we might have read it. We should
-         * skip it if that's the case
-         */
-        if (len != -1 && strncmp(buf, screen_off_max_freq,
-                strlen(screen_off_max_freq)) != 0)
-            memcpy(scaling_max_freq, buf, sizeof(buf));
-        sysfs_write(CPUFREQ_CPU0 "scaling_max_freq", screen_off_max_freq);
-    } else
-        sysfs_write(CPUFREQ_CPU0 "scaling_max_freq", scaling_max_freq);
-
 }
 
 static void aalto_power_hint(struct power_module *module, power_hint_t hint,
@@ -152,11 +183,12 @@ static void aalto_power_hint(struct power_module *module, power_hint_t hint,
                 duration = (int) data;
 
             snprintf(buf, sizeof(buf), "%d", duration);
-            len = write(aalto->boostpulse_fd, "1", 1);
+            len = write(aalto->boostpulse_fd, buf, strlen(buf));
 
             if (len < 0) {
                 strerror_r(errno, buf, sizeof(buf));
                 ALOGE("Error writing to boostpulse: %s\n", buf);
+
                 pthread_mutex_lock(&aalto->lock);
                 close(aalto->boostpulse_fd);
                 aalto->boostpulse_fd = -1;
@@ -174,6 +206,13 @@ static void aalto_power_hint(struct power_module *module, power_hint_t hint,
     }
 }
 
+static void aalto_power_init(struct power_module *module)
+{
+    get_scaling_governor();
+    configure_governor();
+    sysfs_write(CPUFREQ_CPU0 "screen_off_max_freq", "600000");
+}
+
 static struct hw_module_methods_t power_module_methods = {
     .open = NULL,
 };
@@ -185,11 +224,10 @@ struct aalto_power_module HAL_MODULE_INFO_SYM = {
             module_api_version: POWER_MODULE_API_VERSION_0_2,
             hal_api_version: HARDWARE_HAL_API_VERSION,
             id: POWER_HARDWARE_MODULE_ID,
-            name: "Power HAL for Aalto board",
-            author: "Sconosciuto/Androthan",
+            name: "YP-GS1 Power manager",
+            author: "The CyanogenMod Project/Dhiru1602/Androthan",
             methods: &power_module_methods,
         },
-
        init: aalto_power_init,
        setInteractive: aalto_power_set_interactive,
        powerHint: aalto_power_hint,
